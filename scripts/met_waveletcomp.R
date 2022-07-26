96406456378568867fc8e4ab94517ab6d2574016
kristoferhall <khall001@unm.edu>
2022-07-26 12:10:01 -0600

    added daily data to met_waveletcomp.r

--- scripts/met_waveletcomp.R
+++ scripts/met_waveletcomp.R
@@ -13,7 +13,8 @@ library(lubridate)
 library(readr)
 
 
-# starting with monthly data
+# starting with monthly data  ------------------------------------
+
 path_to_files <- "./data/processed_data/"
 
 m <- read_csv(paste0(path_to_files, "met_monthly_gap_filled.csv")) %>% 
@@ -30,7 +31,11 @@ m40 <- m %>%
 
 
 # airt
-my.wt.m40.airt <- analyze.wavelet(m40, "airt", make.pval = TRUE, n.sim = 20)
+my.wt.m40.airt <- analyze.wavelet(m40, 
+                                  my.series = "airt", 
+                                  make.pval = TRUE, 
+                                  n.sim = 200)
+                                  
 
 wt.image(my.wt.m40.airt, 
          main = "Met 40 - Monthly Mean Air Temperature",
@@ -165,7 +170,155 @@ wc.avg(my.wc.ppt_airt, which.avg = "wc",
 
 
 
+#######################################################################
+# daily data ----------------------------------------------------------
 
+d <- read_csv(paste0(path_to_files, "met_daily_gap_filled.csv")) %>% 
+  mutate(sta = as.factor(sta),
+         date = ymd(date))
+
+
+
+m40_d <- d %>% 
+  filter(sta == "40") %>% 
+  select(date, airt, minair, maxair, rh, ppt) %>% 
+  arrange(date)
+
+
+
+# airt
+my.wt.m40_d.airt <- analyze.wavelet(m40_d, 
+                                    "airt", 
+                                    make.pval = TRUE, 
+                                    n.sim = 200)
+
+wt.image(my.wt.m40_d.airt, 
+         main = "Met 40 - Daily Mean Air Temperature",
+         periodlab = "period (Daily)",
+         label.time.axis = TRUE,
+         show.date = TRUE,
+         color.key = "quantile",
+         legend.params = list(label.digits = 3, lab = "power", mar = 8),
+         plot.contour = TRUE)
+
+maximum.level = 1.001 * max(my.wt.m40_d.airt$Power.avg)
+wt.avg(my.wt.m40_d.airt, maximum.level = maximum.level)
+
+
+# maxair
+my.wt.m40_d.maxair <- analyze.wavelet(m40_d, "maxair", make.pval = TRUE, n.sim = 20)
+
+wt.image(my.wt.m40_d.maxair, 
+         main = "Met 40 - Daily Maximum Air Temperature",
+         periodlab = "period (Daily)",
+         label.time.axis = TRUE,
+         show.date = TRUE,
+         color.key = "quantile",
+         legend.params = list(label.digits = 3, lab = "power", mar = 8),
+         plot.contour = TRUE)
+
+maximum.level = 1.001 * max(my.wt.m40_d.maxair$Power.avg)
+wt.avg(my.wt.m40_d.maxair, maximum.level = maximum.level)
+
+
+
+# minair
+my.wt.m40_d.minair <- analyze.wavelet(m40_d, "minair", make.pval = TRUE, n.sim = 20)
+
+wt.image(my.wt.m40_d.minair, 
+         main = "Met 40 - Daily Minimum Air Temperature",
+         periodlab = "period (Daily)",
+         label.time.axis = TRUE,
+         show.date = TRUE,
+         color.key = "quantile",
+         legend.params = list(label.digits = 3, lab = "power", mar = 8),
+         plot.contour = TRUE)
+
+maximum.level = 1.001 * max(my.wt.m40_d.minair$Power.avg)
+wt.avg(my.wt.m40_d.minair, maximum.level = maximum.level)
+
+
+# rh
+my.wt.m40_d.rh <- analyze.wavelet(m40_d, "rh", make.pval = TRUE, n.sim = 20)
+
+wt.image(my.wt.m40_d.rh, 
+         main = "Met 40 - Daily Mean Relative Humidity (%)",
+         periodlab = "period (Daily)",
+         label.time.axis = TRUE,
+         show.date = TRUE,
+         color.key = "quantile",
+         legend.params = list(label.digits = 3, lab = "power", mar = 8),
+         plot.contour = TRUE)
+
+maximum.level = 1.001 * max(my.wt.m40_d.rh$Power.avg)
+wt.avg(my.wt.m40_d.rh, maximum.level = maximum.level)
+
+
+# ppt
+my.wt.m40_d.ppt <- analyze.wavelet(m40_d, "ppt", make.pval = TRUE, n.sim = 20)
+
+wt.image(my.wt.m40_d.ppt, 
+         main = "Met 40 - Daily Total Precipitation (mm)",
+         periodlab = "period (Daily)",
+         label.time.axis = TRUE,
+         show.date = TRUE,
+         color.key = "quantile",
+         legend.params = list(label.digits = 3, lab = "power", mar = 8),
+         plot.contour = TRUE)
+
+maximum.level = 1.001 * max(my.wt.m40_d.ppt$Power.avg)
+wt.avg(my.wt.m40_d.ppt, maximum.level = maximum.level)
+
+
+
+
+
+
+# cross-wavelet for ppt and rh
+m_df <- data.frame(m40_d)
+
+my.wc.ppt_rh <- analyze.coherency(m_df, c("ppt", "rh"), n.sim = 50)
+
+
+wc.image(my.wc.ppt_rh, main = "cross-wavelet power spectrum, precip and rh",
+         legend.params = list(lab = "cross-wavelet power levels"),
+         periodlab = "period (Daily)")
+
+wc.image(my.wc.ppt_rh, main = "cross-wavelet power spectrum, precip and rh",
+         legend.params = list(lab = "cross-wavelet power levels"),
+         periodlab = "period (days)",
+         plot.contour = FALSE) # without contour lines
+
+
+# plot of average coherence:
+wc.avg(my.wc.ppt_rh, which.avg = "wc", 
+       siglvl = 0.05, sigcol = 'red', 
+       legend.coords = "topleft", 
+       periodlab = "period (days)")
+
+
+
+
+# cross-wavelet for ppt and airt
+
+my.wc.ppt_airt <- analyze.coherency(m_df, c("ppt", "airt"), n.sim = 50)
+
+
+wc.image(my.wc.ppt_airt, main = "cross-wavelet power spectrum, precip and airt",
+         legend.params = list(lab = "cross-wavelet power levels"),
+         periodlab = "period (Daily)")
+
+wc.image(my.wc.ppt_airt, main = "cross-wavelet power spectrum, precip and airt",
+         legend.params = list(lab = "cross-wavelet power levels"),
+         periodlab = "period (days)",
+         plot.contour = FALSE) # without contour lines
+
+
+# plot of average coherence:
+wc.avg(my.wc.ppt_airt, which.avg = "wc", 
+       siglvl = 0.05, sigcol = 'red', 
+       legend.coords = "topleft", 
+       periodlab = "period (days)")
 
 
 
